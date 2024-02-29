3ab8e315e4706d53544ef3070ae14f29c5ff7ac6
acaciatang <tst116@ic.ac.uk>
2024-02-29 16:58:29 -0600

    change sample file name and split baseFunctions

--- baseFunctions.py
+++ baseFunctions.py
@@ -7,58 +7,9 @@ __author__ = 'Acacia Tang (ttang53@wisc.edu)'
 __version__ = '0.0.1'
 
 #imports
-import os
-import sys
-import pandas as pd
 import numpy as np
-from scipy import spatial
+from aux import *
 import params
-
-#Called functions
-def restructure_tracking_data(rawOneLR):
-    """Take centroid data from aruco-tracking structured output, rearrange and interpolate missing data"""
-    #Drop any duplicate rows
-    rawOneLR = rawOneLR.drop_duplicates(subset=['ID', 'frame'])
-    xs = rawOneLR.pivot(index="frame", columns='ID', values = ['centroidX', 'centroidY'])
-    return xs.interpolate(method='linear', limit=2, axis='index', limit_direction='both')
-
-def nest_social_center(oneLR):
-    """Generic function to calculate the nest social center from standard pandas array with centroid coordinates."""
-    mean_x = np.nanmean(oneLR['centroidX'].to_numpy())
-    mean_y = np.nanmean(oneLR['centroidY'].to_numpy())
-    return (mean_x, mean_y)
-
-def movement_metrics(oneLR):
-    """Returns two pd.Series(), act is whether a bee is moving in a frame, speed is the speed of a bee in a frame."""
-    speed = np.sqrt(oneLR['centroidX'].diff(axis=0)**2 + oneLR['centroidY'].diff(axis=0)**2)
-    act = speed > params.digital_noise_speed_cutoff
-    act = 1*act
-    act[np.isnan(speed)] = np.nan
-
-    return act, speed
-
-def extract_bee_locs(oneLR, fn):
-    """Restructures coordinates of tag IDs through time."""
-    #Get tracking data
-    xs=oneLR['centroidX'].loc[fn]
-    ys=oneLR['centroidY'].loc[fn]
-    bee_locs = np.column_stack((xs,ys))
-    return bee_locs
-
-def interbee_distance_matrix(oneLR):
-    """Calculates distance between bees through time"""
-    frames = tuple(oneLR.index) #Get unique frames
-    inter_bee_dist=[] #Create empty output array
-    
-    for fn in frames:
-        bee_locs = extract_bee_locs(oneLR, fn)
-        ib_dist = spatial.distance.pdist(bee_locs)
-        ib_dist = spatial.distance.squareform(ib_dist)
-
-        inter_bee_dist.append(ib_dist)
-        
-    return inter_bee_dist
-
 
 #Tests (all tests should be vectorized)
 def trackedFrames(oneLR):
@@ -142,74 +93,3 @@ def totalIntFrames(oneLR):
     countable_frames[self_ind] = 0
 
     return np.nansum(countable_frames, axis=0)
-
-
-def main(argv):
-    """Main entry point of program. For takes in the path to a folder and a list of functions to run. Results will be written to Analysis.csv in the current directory."""
-    output = pd.DataFrame()
-    for dir, subdir, files in os.walk(argv[1]):
-        for f in files:
-            if "mjpeg" in f and os.path.exists(os.path.join(dir,f).replace(".mjpeg", "_updated.csv")):
-                v = os.path.join(dir,f)
-                base = os.path.basename(v)
-                print('Analyzing: ' + base)
-                workerID, Date, Time = base.split("_")
-                Time = Time.replace(".mjpeg", "").replace("-", ":")
-                try:
-                    trackingResults = pd.read_csv(v.replace(".mjpeg", "_updated.csv"))
-                
-                except Exception as e:
-                    print(e)
-                    print("Error: cannot read " + v.replace(".mjpeg", "_updated.csv"))
-                    existingData = [workerID, Date, Time]
-                    addOn = ["" for i in range(len(argv[2:-1])+2)]
-                    row = existingData + addOn
-                    analysis = pd.DataFrame([row], columns = ['pi_ID', 'Date', 'Time', 'LR', 'ID']+argv[2:-1])
-                    analysis.worker = workerID
-                    analysis.Date = Date
-                    analysis.Time = Time
-                    output = pd.concat([output, analysis], ignore_index=True, axis = 0)
-                    continue
-                
-                if argv[-1] == "True":
-                    trackingResults['LR'] = (trackingResults['centroidX'] < np.nanmean(trackingResults['centroidX'].to_numpy()))
-                    trackingResults.loc[trackingResults['LR'], 'LR'] = "Left"
-                    trackingResults.loc[trackingResults['LR'] != "Left", 'LR'] = "Right"
-                else:
-                    trackingResults['LR'] = "Whole"
-                fullAnalysis = pd.DataFrame()
-                datasets = trackingResults.groupby('LR')
-                for name, rawOneLR in datasets:
-                    analysis = pd.DataFrame(index = rawOneLR.ID.unique())
-                    analysis['LR'] = name
-                    analysis['ID'] = analysis.index
-                    oneLR = restructure_tracking_data(rawOneLR)
-                    for test in argv[2:-1]:
-                        try:
-                            analysis[test] = None
-                            analysis[test] = getattr(baseFunctions, test)(oneLR)
-                        except Exception as e:
-                            print(test + " cannot be run on " + v.replace(".mjpeg", ".csv"))
-                            analysis[test] = None
-                            print(e)
-                            break
-                    fullAnalysis = pd.concat([fullAnalysis, analysis], axis = 0)
-                
-                oneVid =  pd.DataFrame(index = fullAnalysis.index)    
-                oneVid['pi_ID'] = workerID
-                oneVid['bee_ID'] = oneVid.index
-                oneVid['Date'] = Date
-                oneVid['Time'] = Time
-                oneVid = pd.concat([oneVid, fullAnalysis], axis = 1)
-
-                output = pd.concat([output, oneVid], ignore_index=True, axis = 0)
-                print('Done!')
-
-    output.to_csv(path_or_buf = "Analysis.csv")
-    print("All done!")
-    return 0
-
-if __name__ == "__main__": 
-    """Makes sure the "main" function is called from command line"""  
-    status = main(sys.argv)
-    sys.exit(status)
--- runMe.py
+++ runMe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Runs functions in baseFunctions.py on dataset. If the brood flag is used, will also run functions in broodFunctions.py"""
+"""Runs all functions in baseFunctions.py on dataset. If the brood flag is used, will also run functions in broodFunctions.py"""
 
 __appname__ = 'runMe.py'
 __author__ = 'Acacia Tang (ttang53@wisc.edu)'
@@ -11,44 +11,68 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+import argparse
+from inspect import getmembers, isfunction
 import baseFunctions
+import broodFunctions
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
+def restructure_tracking_data(rawOneLR):
+    """Take centroid data from aruco-tracking structured output, rearrange and interpolate missing data"""
+    #Drop any duplicate rows
+    rawOneLR = rawOneLR.drop_duplicates(subset=['ID', 'frame'])
+    xs = rawOneLR.pivot(index="frame", columns='ID', values = ['centroidX', 'centroidY'])
+    return xs.interpolate(method='linear', limit=2, axis='index', limit_direction='both')
 
-def main(argv):
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', '-s', type=str, default='.', help='Directory containing data. Defaults to current working directory.')
+    parser.add_argument('--extension', '-e', type=str, default='_updated.csv', help='String at end of all data files from tracking. Defaults to "_updated.csv"')
+    parser.add_argument('--brood', '-b', action='store_true', help='Analyse brood data.')
+    parser.add_argument('--whole', '-w', action='store_true', help='Do not split frame into two when analyzing.')
+    return parser.parse_args()
+
+def main():
     """Main entry point of program. For takes in the path to a folder and a list of functions to run. Results will be written to Analysis.csv in the current directory."""
+    opt = parse_opt()
     output = pd.DataFrame()
-    for dir, subdir, files in os.walk(argv[1]):
+    funcs = [f for f in getmembers(baseFunctions) if isfunction(f[1]) and f[1].__module__ == 'baseFunctions']
+    if vars(opt)['brood'] == True:
+        funcs = funcs + [f for f in getmembers(broodFunctions) if isfunction(f[1]) and f[1].__module__ == 'broodFunctions']
+    
+    for dir, subdir, files in os.walk(vars(opt)['source']):
         for f in files:
-            if "mjpeg" in f and os.path.exists(os.path.join(dir,f).replace(".mjpeg", ".csv")):
+            if "mjpeg" in f and os.path.exists(os.path.join(dir,f).replace(".mjpeg", vars(opt)['extension'])):
                 v = os.path.join(dir,f)
                 base = os.path.basename(v)
                 print('Analyzing: ' + base)
                 workerID, Date, Time = base.split("_")
                 Time = Time.replace(".mjpeg", "").replace("-", ":")
                 try:
-                    trackingResults = pd.read_csv(v.replace(".mjpeg", ".csv"))
+                    trackingResults = pd.read_csv(v.replace(".mjpeg", vars(opt)['extension']))
                 
                 except Exception as e:
                     print(e)
-                    print("Error: cannot read " + v.replace(".mjpeg", ".csv"))
+                    print("Error: cannot read " + v.replace(".mjpeg", vars(opt)['extension']))
                     existingData = [workerID, Date, Time]
-                    addOn = ["" for i in range(len(argv[2:-1])+2)]
+                    addOn = ["" for i in range(len(funcs)+2)]
                     row = existingData + addOn
-                    analysis = pd.DataFrame([row], columns = ['pi_ID', 'Date', 'Time', 'LR', 'ID']+argv[2:-1])
+                    analysis = pd.DataFrame([row], columns = ['pi_ID', 'Date', 'Time', 'LR', 'ID']+test)
                     analysis.worker = workerID
                     analysis.Date = Date
                     analysis.Time = Time
                     output = pd.concat([output, analysis], ignore_index=True, axis = 0)
                     continue
                 
-                if argv[-1] == "True":
+                if vars(opt)['whole']:
+                    trackingResults['LR'] = "Whole"
+                else:
                     trackingResults['LR'] = (trackingResults['centroidX'] < np.nanmean(trackingResults['centroidX'].to_numpy()))
                     trackingResults.loc[trackingResults['LR'], 'LR'] = "Left"
                     trackingResults.loc[trackingResults['LR'] != "Left", 'LR'] = "Right"
-                else:
-                    trackingResults['LR'] = "Whole"
+
                 fullAnalysis = pd.DataFrame()
                 datasets = trackingResults.groupby('LR')
                 for name, rawOneLR in datasets:
@@ -56,12 +80,12 @@ def main(argv):
                     analysis['LR'] = name
                     analysis['ID'] = analysis.index
                     oneLR = restructure_tracking_data(rawOneLR)
-                    for test in argv[2:-1]:
+                    for test in funcs:
                         try:
-                            analysis[test] = None
-                            analysis[test] = getattr(baseFunctions, test)(oneLR)
+                            analysis[test[0]] = None
+                            analysis[test[0]] = test[1](oneLR)
                         except Exception as e:
-                            print(test + " cannot be run on " + v.replace(".mjpeg", ".csv"))
+                            print(test + " cannot be run on " + v.replace(".mjpeg", vars(opt)['extension']))
                             analysis[test] = None
                             print(e)
                             break
@@ -83,5 +107,5 @@ def main(argv):
 
 if __name__ == "__main__": 
     """Makes sure the "main" function is called from command line"""  
-    status = main(sys.argv)
+    status = main()
     sys.exit(status)
