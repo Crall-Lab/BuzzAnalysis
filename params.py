ba6d66f114c207f8907d31de927f6eaed69063fd
Gigi Melone <gmelone@wisc.edu>
2025-10-27 15:39:43 -0500

    sjsdks

--- params.py
+++ params.py
@@ -2,4 +2,5 @@ pixels_per_cm = 172.5
 digital_noise_speed_cutoff = 2.5
 frame_per_sec = 3.7
 interaction_distance_cutoff = 172.5
-onDist = 10
+onDist = 86 
+#gigi changed parameter onDist to 86 pixels from 10 :) 
